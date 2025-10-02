15419eb83bbfede1651473cac19d3e7a7536b2ba
anhth <thabka99@gmail.com>
2025-10-02 09:04:54 +0700

    add 20 question

---
+++ questions_251_270.js
@@ -0,0 +1,202 @@
+const questions = [
+  {
+    "question": "Đối với đấu thầu qua mạng, thành phần nào sau đây không được coi là một phần của E-HSMST, E-HSMT và nhà thầu không phải đáp ứng các yêu cầu này?",
+    "options": [
+      "Bảng dữ liệu được số hóa dưới dạng webform trên Hệ thống mạng đấu thầu quốc gia",
+      "Tiêu chuẩn đánh giá về tính hợp lệ được đính kèm trên Hệ thống mạng đấu thầu quốc gia",
+      "Yêu cầu về năng lực, kinh nghiệm được đính kèm trên Hệ thống mạng đấu thầu quốc gia",
+      "Phương án B và C đều đúng"
+    ],
+    "correct": "D"
+  },
+  {
+    "question": "Yêu cầu nào sau đây là đúng đối với tệp tin (file) đăng tải trên Hệ thống mạng đấu thầu quốc gia?",
+    "options": [
+      "Các file mở, đọc được bằng các phần mềm thông dụng như: các phần mềm đọc, soạn thảo văn bản MS Office hoặc Open Office; các phần mềm đọc file PDF; các phần mềm thiết kế thông dụng như AutoCad, Photoshop; phần mềm đọc file ảnh tích hợp sẵn trên Hệ điều hành Windows. Các file sử dụng phông chữ không thuộc bảng mã Unicode",
+      "Các file nén mở được bằng các phần mềm giải nén thông dụng như phần mềm giải nén ZIP tích hợp sẵn trên hệ điều hành Windows, MacOS hoặc phần mềm giải nén Rar hoặc 7Zip. Trường hợp sử dụng file nén, các file sau khi giải nén phải có định dạng đúng quy định.",
+      "Không bị nhiễm virus, không bị lỗi, hỏng và không thiết lập mật khẩu",
+      "Các phương án trên đều đúng"
+    ],
+    "correct": "D"
+  },
+  {
+    "question": "Đối với đấu thầu qua mạng, trường hợp file đính kèm trong E- HSMT không thể mở hoặc không đọc được thì chủ đầu tư phải thực hiện hành động nào sau đây?",
+    "options": [
+      "Đăng tải và phát hành lại toàn bộ E-HSMT",
+      "Sửa đổi E-HSMT và không phải phát hành lại toàn bộ E-HSMT",
+      "Đề nghị Trung tâm Đấu thầu qua mạng quốc gia sửa đổi E-HSMT trong trường hợp chủ đầu tư không sửa đổi được E-HSMT",
+      "Các phương án trên đều đúng"
+    ],
+    "correct": "A"
+  },
+  {
+    "question": "Đối với đấu thầu qua mạng, trường hợp Hệ thống mạng đấu thầu quốc gia gặp sự cố không thể vận hành và phải tự động gia hạn thời điểm đóng thầu của các gói thầu bị ảnh hưởng (có thời điểm đóng thầu trong thời gian từ khi Hệ thống bị sự cố cho đến thời điểm sau hoàn thành khắc phục sự cố 02 giờ) thì việc đánh giá E-HSDT được thực hiện trên cơ sở thời điểm đóng thầu nào sau đây?",
+    "options": [
+      "Thời điểm đóng thầu nêu trong E-TBMT được đăng tải (trước thời điểm Hệ thống mạng đấu thầu quốc gia gặp sự cố)",
+      "Thời điểm đóng thầu mà Hệ thống mạng đấu thầu quốc gia tự động gia hạn",
+      "Do Chủ đầu tư quyết định",
+      "Phương án A và B đều đúng"
+    ],
+    "correct": "D"
+  },
+  {
+    "question": "Loại Chứng thư số nào sau đây được sử dụng trên Hệ thống mạng đấu thầu quốc gia?",
+    "options": [
+      "Chứng thư số chuyên dùng do tất cả tổ chức có chức năng cung cấp dịch vụ chứng thực chữ ký số chuyên dùng cấp",
+      "Tất cả các loại chứng thư số",
+      "Chứng thư số công cộng do tổ chức cung cấp dịch vụ chứng thực chữ ký số công cộng cấp hoặc chứng thư số do tổ chức cung cấp dịch vụ chứng thực chữ ký số chuyên dùng Chính phủ cấp",
+      "Phương án A và C đều đúng"
+    ],
+    "correct": "C"
+  },
+  {
+    "question": "Đối với đấu thầu không qua mạng, việc phê duyệt và phát hành hồ sơ mời thầu được thực hiện theo phương án nào sau đây?",
+    "options": [
+      "Phê duyệt và phát hành hồ sơ mời thầu trên Hệ thống mạng đấu thầu quốc gia",
+      "Phê duyệt trên Hệ thống mạng đấu thầu quốc gia và phát hành hồ sơ mời thầu bản giấy",
+      "Phê duyệt và phát hành hồ sơ mời thầu bản giấy",
+      "Các phương án trên đều đúng"
+    ],
+    "correct": "A"
+  },
+  {
+    "question": "Đối với đấu thầu qua mạng, trường hợp nhà thầu sử dụng bảo lãnh dự thầu điện tử trên Hệ thống mạng đấu thầu quốc gia, phát biểu nào sau đây là đúng?",
+    "options": [
+      "Giá trị bảo lãnh dự thầu luôn đáp ứng yêu cầu trong E-HSMT",
+      "Thời gian hiệu lực của bảo lãnh dự thầu luôn đáp ứng yêu cầu trong E-HSMT",
+      "Đối tượng thụ hưởng bảo lãnh dự thầu luôn đáp ứng yêu cầu trong E-HSMT",
+      "Các phương án trên đều đúng"
+    ],
+    "correct": "D"
+  },
+  {
+    "question": "Đối với đấu thầu qua mạng, trường hợp nhà thầu rút E-HSDT sau thời điểm đóng thầu và trong thời gian có hiệu lực của E-HSDT thì nhà thầu bị xử lý theo phương án nào sau đây?",
+    "options": [
+      "Không được hoàn trả bảo đảm dự thầu",
+      "Bị đánh giá là không đảm bảo uy tín khi tham dự thầu",
+      "Không được tiếp tục đánh giá E-HSDT",
+      "Các phương án trên đều đúng"
+    ],
+    "correct": "D"
+  },
+  {
+    "question": "Đối với đấu thầu qua mạng, sau khi đánh giá E-HSDT, tổ trưởng tổ chuyên gia đính kèm bản scan báo cáo đánh giá E-HSDT lên Hệ thống mạng đấu thầu quốc gia với yêu cầu nào sau đây?",
+    "options": [
+      "Báo cáo đánh giá E-HSDT có chữ ký của tất cả thành viên trong tổ chuyên gia",
+      "Báo cáo đánh giá E-HSDT chỉ cần có chữ ký của tổ trưởng tổ chuyên gia",
+      "Báo cáo đánh giá E-HSDT (đăng tải báo cáo đánh giá tổng hợp, không cần kèm theo các phiếu chấm của thành viên trong tổ chuyên gia)",
+      "Phương án B và C đều đúng"
+    ],
+    "correct": "A"
+  },
+  {
+    "question": "Đối với đấu thầu qua mạng, trường hợp E-HSMT có yêu cầu cung cấp hàng mẫu, nhà thầu có thể nộp bổ sung hàng mẫu theo phương án nào sau đây?",
+    "options": [
+      "Trong thời hạn 03 ngày làm việc sau thời điểm đóng thầu",
+      "Trong thời hạn 03 ngày làm việc sau thời điểm mở thầu",
+      "Trong thời hạn 05 ngày làm việc sau thời điểm đóng thầu",
+      "Trong thời hạn 05 ngày làm việc sau thời điểm mở thầu"
+    ],
+    "correct": "C"
+  },
+  {
+    "question": "Trường hợp nào sau đây, nhà thầu được đánh giá là không đáp ứng quy định về bảo đảm cạnh tranh khi tham dự thầu gói thầu tổ chức đấu thầu rộng rãi?",
+    "options": [
+      "Đơn vị sự nghiệp công lập A thuộc Sở Tài chính tỉnh X tham gia gói thầu do Sở Tài chính tỉnh X là chủ đầu tư mà chức năng, nhiệm vụ của đơn vị sự nghiệp công lập A phù hợp với gói thầu",
+      "Đơn vị sự nghiệp công lập A tham gia gói thầu do Chi cục B là chủ đầu tư. Chức năng nhiệm vụ của đơn vị sự nghiệp công lập A phù hợp với tính chất gói thầu. Đơn vị sự nghiệp công lập A và Chi cục B lần lượt là đơn vị sự nghiệp công lập và đơn vị hành chính do Sở Tài Chính tỉnh X trực tiếp quản lý",
+      "Đơn vị sự nghiệp công lập A tham gia gói thầu do đơn vị sự nghiệp công lập B là chủ đầu tư và cả hai đơn vị đều là đơn vị sự nghiệp do Sở Tài chính tỉnh X trực tiếp quản lý",
+      "Doanh nghiệp C tham gia gói thầu có sử dụng vốn ngân sách nhà nước do đơn vị sự nghiệp công lập A là chủ đầu tư. Đơn vị sự nghiệp công lập A và Doanh nghiệp C đều là đơn vị do Sở X trực tiếp quản lý"
+    ],
+    "correct": ""
+  },
+  {
+    "question": "Đối với các gói thầu đã thanh lý hợp đồng, khi lập Tờ trình Kế hoạch lựa chọn nhà thầu, giá trị phần công việc đã thực hiện của các gói thầu này được tính như thế nào?",
+    "options": [
+      "Giá gói thầu",
+      "Giá trị nghiệm thu hợp đồng",
+      "Giá hợp đồng",
+      "Có thể tùy chọn, cần căn cứ theo giá gói thầu, giá hợp đồng hoặc giá trị nghiệm thu hợp đồng để xác định"
+    ],
+    "correct": "B"
+  },
+  {
+    "question": "Trường hợp nhà thầu bị đánh giá về uy tín khi thực hiện hành vi: được lựa chọn trúng thầu nhưng không tiến hành hoặc từ chối tiến hành hoàn thiện hợp đồng hoặc không ký kết hợp đồng thì khi tham gia gói thầu khác, nhà thầu sẽ bị bất lợi như thế nào?",
+    "options": [
+      "Đối với gói thầu hàng hóa, xây lắp, phi tư vấn, hỗn hợp, nhà thầu bị đánh giá về uy tín phải nộp gấp 3 lần bảo đảm dự thầu trong vòng 03 năm kể từ lần cuối cùng thực hiện hành vi",
+      "Đối với gói thầu tư vấn, thông tin uy tín này sẽ được sử dụng để đánh giá về kỹ thuật",
+      "Bị đưa vào xem xét, đánh giá về kỹ thuật trong quá trình lựa chọn nhà thầu đối với gói thầu xây lắp, phi tư vấn, hàng hóa, hỗn hợp",
+      "Đáp án B và C đều đúng"
+    ],
+    "correct": "D"
+  },
+  {
+    "question": "Nội dung nào không phù hợp với quy định về nhà thầu phụ đối với gói thầu mua sắm hàng hóa?",
+    "options": [
+      "Chủ đầu tư phải ghi tỷ lệ % giá trị dành cho nhà thầu phụ trong E-BDL làm cơ sở để nhà thầu lập E-HSDT",
+      "Năng lực và kinh nghiệm của nhà thầu phụ sẽ không được xem xét khi đánh giá E-HSDT của nhà thầu",
+      "Nhà thầu được ký kết hợp đồng với các nhà thầu phụ trong danh sách các nhà thầu phụ nêu trong E-HSDT",
+      "Nhà thầu được ký kết hợp đồng với các nhà thầu phụ được chủ đầu tư chấp thuận để tham gia thực hiện cung cấp dịch vụ liên quan"
+    ],
+    "correct": "A"
+  },
+  {
+    "question": "Nhà thầu có được tham dự thầu với tư cách nhà thầu liên danh trong trường hợp phạm vi công việc của gói thầu mua sắm hàng hóa chỉ gồm 01 hạng mục là hệ thống máy chủ không?",
+    "options": [
+      "Được liên danh nếu nhà thầu phân chia công việc trong liên danh theo các công việc thuộc quá trình sản xuất hạng mục trong bảng giá dự thầu",
+      "Không được liên danh, nhà thầu chỉ được tham dự với tư cách độc lập do phạm vi công việc chỉ gồm 01 hạng mục",
+      "Được liên danh trong trường hợp 01 thành viên thực hiện toàn bộ hạng mục hệ thống máy chủ, thành viên còn lại thực hiện thu xếp tài chính cho gói thầu",
+      "Tùy thuộc vào quy định của hồ sơ mời thầu"
+    ],
+    "correct": "A"
+  },
+  {
+    "question": "Có 4 nhà thầu tham dự thầu gói thầu mua sắm hàng hóa áp dụng phương pháp giá thấp nhất, trong đó: - Nhà thầu A, B, D là doanh nghiệp nhỏ và vừa do phụ nữ làm chủ. - Nhà thầu A, B, C và D có giá dự thầu sau sửa lỗi, hiệu chỉnh sai lệch, trừ đi giá trị giảm giá lần lượt là: 95 tỷ đồng, 100 tỷ đồng, 100 tỷ đồng và 105 tỷ đồng. - Nhà thầu A, B, C và D chứng minh được hàng hóa mình tham dự có tỷ lệ chi phí sản xuất trong nước lần lượt là: 20%, 40%, 45% và 60%. Trong trường hợp tất cả nhà thầu đều đáp ứng về kỹ thuật thì nhà thầu nào được xét duyệt trúng thầu?",
+    "options": [
+      "Nhà thầu A được xét duyệt trúng thầu",
+      "Nhà thầu B được xét duyệt trúng thầu",
+      "Nhà thầu C được xét duyệt trúng thầu",
+      "Nhà thầu D được xét duyệt trúng thầu"
+    ],
+    "correct": "A"
+  },
+  {
+    "question": "Trong lễ mở thầu đối với gói thầu đấu thầu quốc tế không qua mạng, chủ đầu tư xử lý như thế nào khi nhà thầu đề nghị bổ sung thư giảm giá do bị thất lạc trong quá trình nộp hồ sơ dự thầu theo đường bưu điện?",
+    "options": [
+      "Chủ đầu tư chấp nhận do đây là lỗi vận chuyển theo bưu điện",
+      "Chủ đầu tư không chấp nhận vì thư giảm giá của nhà thầu nộp sau thời điểm đóng thầu",
+      "Chủ đầu tư tạm thời ghi nhận nội dung trong thư giảm giá của nhà thầu và trình Người có thẩm quyền quyết định",
+      "Chủ đầu tư coi là tình huống trong đấu thầu và xử lý trên cơ sở bảo đảm cạnh tranh, công bằng, minh bạch, hiệu quả kinh tế và trách nhiệm giải trình"
+    ],
+    "correct": "B"
+  },
+  {
+    "question": "Nhận định nào sau đây là đúng ?",
+    "options": [
+      "Gói thầu xây lắp, hàng hóa và dịch vụ phi tư vấn áp dụng hình thức chào hàng cạnh tranh và sử dụng phương pháp dựa trên kỹ thuật để đánh giá về tài chính và có giá đề nghị trúng thầu vượt giá gói thầu được phép thương thảo về chi phí",
+      "Gói thầu mua thuốc áp dụng phương thức một giai đoạn hai túi hồ sơ và sử dụng phương pháp kết hợp giữa kỹ thuật và giá được phép thương thảo về chi phí trong quá trình thương thảo hợp đồng",
+      "Gói thầu dịch vụ tư vấn áp dụng hình thức đấu thầu rộng rãi được phép thương thảo về chi phí trong quá trình thương thảo hợp đồng",
+      "Không được phép thương thảo về chi phí trong quá trình thương thảo hợp đồng đối với tất cả các gói thầu, trừ trường hợp nhà thầu tự nguyện giảm giá π Nội dung Phương án trả lời"
+    ],
+    "correct": "C"
+  },
+  {
+    "question": "Hợp đồng nào được phép sử dụng để chứng minh hợp đồng tương tự khi tham dự gói thầu cung cấp thuốc có sử dụng ngân sách nhà nước của Bệnh viện?",
+    "options": [
+      "Hợp đồng thuốc đã cung cấp thuốc cho cơ sở khám chữa bệnh tư nhân",
+      "Hợp đồng thuốc đã cung cấp thuốc cho các cơ sở kinh doanh thuốc",
+      "Phương án A và B đều đúng",
+      "Phương án A và B đều sai"
+    ],
+    "correct": "C"
+  },
+  {
+    "question": "Gói thầu mua sắm hàng hóa có: - Thời điểm đóng thầu theo E-TBMT là: 8h00 ngày 25/9/2025 - Hiệu lực của bảo đảm dự thầu theo yêu cầu của E-HSMT là: 90 ngày - Do lỗi hệ thống nên Hệ thống tự động gia hạn thời điểm đóng thầu mới là: 11h ngày 26/9/2025 - Nhà thầu A đã nộp E-HSDT trước thời điểm Hệ thống xảy ra sự cố với hiệu lực của bảo đảm dự thầu là 90 ngày kể từ ngày 25/9/2025; Nhà thầu B nộp E-HSDT sau thời điểm Hệ thống xảy ra sự cố và trước thời điểm đóng thầu mới với hiệu lực của bảo đảm dự thầu là 90 ngày kể từ ngày 26/9/2025 Bảo đảm dự thầu của nhà thầu A và nhà thầu B được đánh giá như thế nào?",
+    "options": [
+      "Bảo đảm dự thầu của nhà thầu A được đánh giá là không hợp lệ; Bảo đảm dự thầu của nhà thầu B được đánh giá là hợp lệ",
+      "Bảo đảm dự thầu của nhà thầu A và nhà thầu B đều được đánh giá là hợp lệ",
+      "Bảo đảm dự thầu của nhà thầu A được đánh giá là hợp lệ; Bảo đảm dự thầu của nhà thầu B được đánh giá là không hợp lệ",
+      "Bảo đảm dự thầu của nhà thầu A và nhà thầu B đều được đánh giá là không hợp lệ"
+    ],
+    "correct": "C"
+  }
+];
